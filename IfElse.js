250ef56540b5816837360d8b7eb426ae27b231c8
Brenda Nzyuko <mbutubrenda97@gmail.com>
2018-02-17 12:50:35 +0300

    learnt JS

---
+++ IfElse.js
@@ -0,0 +1,9 @@
+var isSunday = false 
+
+if(isSunday){
+	console.log("watch movie")
+}
+
+else{
+	console.log("learn code")
+}
---
+++ variables.js
@@ -0,0 +1,7 @@
+var classSize = 23;
+var isWeekDay = false;
+var className ="Mobile development"
+var shoeSize = 4.1
+// This is used to print
+
+console.log (classSize)log 
