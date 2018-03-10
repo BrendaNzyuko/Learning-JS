1d4f36314209bc720cb2adef0071b17dee4d8526
Brenda Nzyuko <mbutubrenda97@gmail.com>
2018-03-10 11:52:51 +0300

    arrays and parameters

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+ var student = ["Jane","Lauren","Ivy","Fatma","Judith","Irene","Mary"]
+ var shoeSize = [12,34,45,37,40,34,40]
+
+ //get the length of array
+
+ console.log(student)
+for(i = 0;i<student.length;i+=1)
+	
+	console.log(student[i])
---
+++ FunctionParameter.js
@@ -0,0 +1,16 @@
+function areaOfTriangle(base,height){
+	
+	var area = 0.5*base*height;
+return area;
+}
+ var answer =areaOfTriangle(70,10)
+var result =areaOfTriangle(50,40)
+var results =areaOfTriangle(60,40)
+
+print (result)
+print(answer)
+print(results)
+
+function print(answer){
+console.log("area Of Triangle"+ answer)
+}
